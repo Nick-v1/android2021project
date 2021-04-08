fc829196745a2837baef8bf1e547ea5369e1148f
Nick <nikos.voulgaridis1@gmail.com>
2021-04-08 23:09:09 +0300

    Second commit

--- app/src/main/java/advhci/semester/androidproject/LauncherActivity.java
+++ app/src/main/java/advhci/semester/androidproject/LauncherActivity.java
@@ -2,16 +2,13 @@ package advhci.semester.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import java.util.concurrent.Delayed;
-
-public class MainActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity {
 
 
 
@@ -19,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launcher);
 
         Button button = findViewById(R.id.button);
         ImageButton imageButton = findViewById(R.id.imageButton);
