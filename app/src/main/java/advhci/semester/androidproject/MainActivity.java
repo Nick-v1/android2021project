d6962fbaf2fb44e2bcc9b20dc82887cc3b255222
Nick <nikos.voulgaridis1@gmail.com>
2021-04-08 22:18:14 +0300

    First commit

---
+++ app/src/main/java/advhci/semester/androidproject/MainActivity.java
@@ -0,0 +1,51 @@
+package advhci.semester.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.concurrent.Delayed;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        ImageButton imageButton = findViewById(R.id.imageButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getActivity();
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getActivity();
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    protected Intent getActivity(){
+        return new Intent(this, FirstActivity.class);
+    }
+
+
+}
