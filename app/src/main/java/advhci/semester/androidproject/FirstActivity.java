ebc26d8e7a295bcfa53aebfc23e63eef97424be4
Nick <nikos.voulgaridis1@gmail.com>
2021-04-09 21:35:09 +0300

    Exit app button work!!

--- app/src/main/java/advhci/semester/androidproject/FirstActivity.java
+++ app/src/main/java/advhci/semester/androidproject/FirstActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -11,6 +17,23 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        ImageButton exitbutton = findViewById(R.id.btmBarExit);
+
+        exitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        TextView exittext = findViewById(R.id.Exit);
+        exittext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 
