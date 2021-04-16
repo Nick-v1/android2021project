1ebc021684c9ced3d274abf107706089616c6071
Nick-v1 <nikos.voulgaridis1@gmail.com>
2021-04-16 21:34:42 +0300

    drawer menu

--- app/src/main/java/advhci/semester/androidproject/FirstActivity.java
+++ app/src/main/java/advhci/semester/androidproject/FirstActivity.java
@@ -1,37 +1,35 @@
 package advhci.semester.androidproject;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
-
-import org.w3c.dom.Text;
 
 public class FirstActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar topAppBar;
+    public static FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
 
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.NavMenuView);
@@ -41,11 +39,13 @@ public class FirstActivity extends AppCompatActivity {
         View toolBarExitApp = findViewById(R.id.appbarExit);
         toolBarExitApp.setOnClickListener(this::endApp);              //toolbar exit button
 
-        TextView drawerMenuExitView = findViewById(R.id.Exit);
-        drawerMenuExitView.setOnClickListener(this::endApp);         //drawer exit
+
 
         topAppBar = findViewById(R.id.topAppBar);          //drawer menu button
         topAppBar.setNavigationOnClickListener(this::openDrawer);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 
     private void openDrawer(View view) {
@@ -58,12 +58,17 @@ public class FirstActivity extends AppCompatActivity {
     }
 //drawer menu effects and coding here
     public boolean navItemSelected(MenuItem item){
-        if(item.getItemId() == R.id.itemDrawerAdd){
+        if(item.getItemId() == R.id.itemDrawerAddSport){
+
             item.setChecked(true);
             drawerLayout.closeDrawers();
+
+            fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, new InsertSportFragment()).commit();
+
             // code here
             return true;
-        }else if (item.getItemId() == R.id.itemDrawerDelete){
+        }else if (item.getItemId() == R.id.itemDrawerDeleteSport){
             item.setChecked(true);
             drawerLayout.closeDrawers();
             //
@@ -71,6 +76,8 @@ public class FirstActivity extends AppCompatActivity {
         } else if (item.getItemId() == R.id.itemDrawerHome){
             item.setChecked(true);
             drawerLayout.closeDrawers();
+            fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             //
             return true;
         } else if (item.getItemId() == R.id.itemDrawerInfo){
@@ -83,11 +90,14 @@ public class FirstActivity extends AppCompatActivity {
             drawerLayout.closeDrawers();
             //
             return true;
-        } else if (item.getItemId() == R.id.itemDrawerUpdate){
+        } else if (item.getItemId() == R.id.itemDrawerUpdateAthlete){
             item.setChecked(true);
             drawerLayout.closeDrawers();
             //
             return true;
+        } else if (item.getItemId() == R.id.ExitAppDrawer){
+            finish();
+            Toast.makeText(getApplicationContext(), "Exited App", Toast.LENGTH_SHORT).show();
         }
         return false;
     }
