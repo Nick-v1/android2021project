c8f5b8d790de8d7a4debd7fdc98fb396987375cd
Nick-v1 <nikos.voulgaridis1@gmail.com>
2021-04-18 17:51:46 +0300

    shorter FirstActivity code

--- app/src/main/java/advhci/semester/androidproject/FirstActivity.java
+++ app/src/main/java/advhci/semester/androidproject/FirstActivity.java
@@ -130,7 +130,6 @@ public class FirstActivity extends AppCompatActivity {
         }
         else{
             item.setChecked(false);
-            return false;
         }
 
         return false;
